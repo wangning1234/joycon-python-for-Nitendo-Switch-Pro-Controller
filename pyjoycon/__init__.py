abdb03886d55518cf88f46ae8016a5302191e2bf
wangning1234 <408083375@qq.com>
2024-11-20 16:06:07 +0800

    Update __init__.py
    increase the ProController Class

--- pyjoycon/__init__.py
+++ pyjoycon/__init__.py
@@ -1,12 +1,14 @@
 from .joycon import JoyCon
-from .wrappers import PythonicJoyCon  # as JoyCon
+from .wrappers import PythonicJoyCon
 from .gyro import GyroTrackingJoyCon
 from .event import ButtonEventJoyCon
-from .device import get_device_ids, get_ids_of_type
-from .device import is_id_L
-from .device import get_R_ids, get_L_ids, get_PRO_ids
-from .device import get_R_id, get_L_id, get_PRO_id
-
+from .device import (
+    get_device_ids, get_ids_of_type,
+    is_id_L,
+    get_R_ids, get_L_ids, get_PRO_ids,
+    get_R_id, get_L_id, get_PRO_id
+)
+from .procon import ProController
 
 __version__ = "0.2.4"
 
@@ -15,6 +17,7 @@ __all__ = [
     "GyroTrackingJoyCon",
     "JoyCon",
     "PythonicJoyCon",
+    "ProController",
     "get_L_id",
     "get_L_ids",
     "get_R_id",
